f6ca03414f83ecd34ff1e4051754cce0606bdaf8
ghostnotes <ghostnotesdot@gmail.com>
2013-10-27 01:39:22 +0900

    Added social button of Facebook and Hatena.

--- app/assets/javascripts/feeds.js.coffee
+++ app/assets/javascripts/feeds.js.coffee
@@ -1,3 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+do ->
+  window.onload= ->
+    $('.hatena-bookmark-button-frame').addClass('hatena-like-ex')
+    return
--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -2,4 +2,23 @@ module FeedsHelper
   def create_publish_date(date)
     DateTime.parse(date).strftime('%d/%m/%Y %H:%M')
   end
+
+  def get_facebook_script_tag
+    '<div id="fb-root"></div>
+     <script>(function(d, s, id) {
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) return;
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+       fjs.parentNode.insertBefore(js, fjs);
+    }(document, \'script\', \'facebook-jssdk\'));</script>'.html_safe
+  end
+
+  def get_facebook_like_tag(url)
+    "<div class=\"fb-like fb-like-ex\" data-href=\"#{url}\" data-width=\"The pixel width of the plugin\" data-height=\"The pixel height of the plugin\" data-colorscheme=\"light\" data-layout=\"button_count\" data-action=\"like\" data-show-faces=\"false\" data-send=\"false\"></div>".html_safe
+  end
+
+  def get_hatena_bookmark_tag(title, url)
+    "<a href=\"http://b.hatena.ne.jp/entry/#{url}\" class=\"hatena-bookmark-button\" data-hatena-bookmark-title=\"#{title}\" data-hatena-bookmark-layout=\"simple-balloon\" title=\"このエントリーをはてなブックマークに追加\"><img src=\"http://b.st-hatena.com/images/entry-button/button-only@2x.png\" alt=\"このエントリーをはてなブックマークに追加\" width=\"20\" height=\"25\" style=\"border: none;\" /></a><script type=\"text/javascript\" src=\"http://b.st-hatena.com/js/bookmark_button.js\" charset=\"utf-8\" async=\"async\"></script>".html_safe
+  end
 end
